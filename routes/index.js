78ac685a1527bb8760b58b5fc6754ce009a88e32
Klint Tiley <kctiley@gmail.com>
2015-08-28 20:05:21 -0600

    Text input and submit working to output reformatted text on blank page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'This is my page now!' });
 });
 
+// router.post('/foo/bar', function (req, res, next) {
+//   res.send('Submit has been clicked and router posted to path foo/bar')
+// })
+
+router.post('/foo/bar', function (req, res, next) { res.send(req.body.emotion + " " + req.body.level) })
+
 module.exports = router;
