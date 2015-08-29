7f6f80c7eb3f042b5bdfea83d49c896b3b3544cd
Klint Tiley <kctiley@gmail.com>
2015-08-29 13:12:56 -0600

    Returns input text plus random index from snippets ipsum.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var ipsumArr = require('../lib/snippets.js');
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hypstafy By Klint' });
+  res.render('index', {title: 'HYPSATAFY By Klint', subtitle: 'How to be super hipsta-licious!'});
 });
 
-// router.post('/foo/bar', function (req, res, next) {
-//   res.send('Submit has been clicked and router posted to path foo/bar')
-// })
+var randomIpsumIndex = Math.floor(Math.random() * 17);
+//router.post('/foo/bar', function (req, res, next) { res.send(req.body.sentence + " something random ")})
+router.post('/foo/bar', function (req, res, next) { 
+  res.send(req.body.sentence + ipsumArr[randomIpsumIndex])})
 
-router.post('/foo/bar', function (req, res, next) { res.send(req.body.sentence + " something random ") })
+
 
 module.exports = router;
