b816d7a0d3c7bc4aec41cbb522e7297e87862b6d
Klint Tiley <kctiley@gmail.com>
2015-08-28 20:31:06 -0600

    Removed extra input box and changed some text

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is my page now!' });
+  res.render('index', { title: 'Hypstafy By Klint' });
 });
 
 // router.post('/foo/bar', function (req, res, next) {
 //   res.send('Submit has been clicked and router posted to path foo/bar')
 // })
 
-router.post('/foo/bar', function (req, res, next) { res.send(req.body.emotion + " " + req.body.level) })
+router.post('/foo/bar', function (req, res, next) { res.send(req.body.sentence + " something random ") })
 
 module.exports = router;
